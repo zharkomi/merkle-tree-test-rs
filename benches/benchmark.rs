98779eb9f41c303c46e52bd512e8cd75224a2b8d
mikhail <mikhail@qwe.com>
2019-02-21 20:22:09 +0300

    vmt tests

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,7 +1,9 @@
-#[macro_use]
 extern crate criterion;
 extern crate mtt;
 extern crate rand;
+
+
+use std::collections::HashMap;
 
 use criterion::Criterion;
 
@@ -10,13 +12,43 @@ use mtt::VmtWrapper;
 
 static STEP_SIZE: usize = 500;
 
-fn criterion_benchmark(c: &mut Criterion, counts: Vec<usize>, test_data: Vec<Vec<String>>, tree_factory: &'static Fn(&Vec<String>) -> VmtWrapper) {
-    c.bench_function_over_inputs("Vector Merkle Tree",
-                                 move |b, i| {
-                                     b.iter(|| tree_factory(&test_data[*i / STEP_SIZE - 1]));
-                                 },
-                                 counts,
-    );
+fn benchmark_constructor(c: &mut Criterion, counts: Vec<usize>, test_data: Vec<Vec<String>>,
+                         wrapper_map: &HashMap<String, &'static Fn(&Vec<String>) -> VmtWrapper>) {
+    for (key, tree_factory) in wrapper_map {
+        let data: Vec<Vec<String>> = test_data.clone();
+        let mut title = key.clone();
+        title.push_str(" Creation");
+        let factory = *tree_factory;
+        c.bench_function_over_inputs(title.as_str(),
+                                     move |b, i| {
+                                         b.iter(|| factory(&data[*i / STEP_SIZE - 1]));
+                                     },
+                                     counts.clone(),
+        );
+    }
+}
+
+fn benchmark_method(c: &mut Criterion, counts: Vec<usize>, test_data: Vec<Vec<String>>,
+                    wrapper_map: &HashMap<String, &'static Fn(&Vec<String>) -> VmtWrapper>) {
+    for (key, tree_factory) in wrapper_map {
+        let data: Vec<Vec<String>> = test_data.clone();
+        let mut title = key.clone();
+        title.push_str(" Find");
+        let mut trees: Vec<VmtWrapper> = vec![];
+        for d in &data {
+            trees.push(tree_factory(d));
+        }
+        c.bench_function_over_inputs(title.as_str(),
+                                     move |b, i| {
+                                         b.iter(|| {
+                                             let index = *i / STEP_SIZE - 1;
+                                             let item_index = rand::random::<usize>() % data[index].len();
+                                             trees[index].find(&data[index][item_index]);
+                                         });
+                                     },
+                                     counts.clone(),
+        );
+    }
 }
 
 fn generate_blocks(counts: &Vec<usize>) -> Vec<Vec<String>> {
@@ -35,13 +67,31 @@ fn generate_blocks(counts: &Vec<usize>) -> Vec<Vec<String>> {
     result
 }
 
-pub fn creation_benches() {
-    let counts = (1..7).map(|i| i * STEP_SIZE).collect();
-    let test_data: Vec<Vec<String>> = generate_blocks(&counts);
-    let mut criterion: ::criterion::Criterion =
-        ::criterion::Criterion::default().configure_from_args();
-    criterion_benchmark(&mut criterion, counts, test_data, &VmtWrapper::create_tree);
+pub fn creation_benches(wrapper_map: &HashMap<String, &'static Fn(&Vec<String>) -> VmtWrapper>) {
+    let (counts, test_data, mut criterion) = prepare_test();
+    benchmark_constructor(&mut criterion, counts, test_data, wrapper_map);
 }
 
-//criterion_group!(benches, criterion_benchmark);
-criterion_main!(creation_benches);
+pub fn find_benches(wrapper_map: &HashMap<String, &'static Fn(&Vec<String>) -> VmtWrapper>) {
+    let (counts, test_data, mut criterion) = prepare_test();
+    benchmark_method(&mut criterion, counts, test_data, wrapper_map);
+}
+
+fn prepare_test() -> (Vec<usize>, Vec<Vec<String>>, Criterion) {
+    let counts: Vec<usize> = (1..7).map(|i| i * STEP_SIZE).collect();
+    let test_data: Vec<Vec<String>> = generate_blocks(&counts);
+    let criterion: ::criterion::Criterion =
+        ::criterion::Criterion::default().configure_from_args();
+    (counts, test_data, criterion)
+}
+
+
+fn main() {
+    let mut wrapper_map: HashMap<String, &'static Fn(&Vec<String>) -> VmtWrapper> = HashMap::new();
+    wrapper_map.insert(String::from("Vector Merkle Tree"), &VmtWrapper::create_tree);
+    wrapper_map.insert(String::from("Vector Merkle Tree With Map"), &VmtWrapper::create_tree_with_map);
+
+    find_benches(&wrapper_map);
+    creation_benches(&wrapper_map);
+    ::criterion::Criterion::default().configure_from_args().final_summary();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,41 @@
+extern crate criterion;
 extern crate ring;
 
 use ring::digest::{Algorithm, SHA512};
 
 pub trait TreeWrapper<V> {
-    fn find(value: &V);
-    fn find_and_validate(value: &V);
+    fn find(&self, value: &V);
+    fn find_and_validate(&self, value: &V);
 }
 
 static ALGO: &'static Algorithm = &SHA512;
 
 pub struct VmtWrapper {
-    tree: vmt::MerkleTree<String>
+    tree: vmt::MerkleTree,
+    root: Vec<u8>,
 }
 
 impl VmtWrapper {
     pub fn create_tree(data: &Vec<String>) -> VmtWrapper {
-        VmtWrapper { tree: vmt::MerkleTree::new(data, ALGO) }
+        let tree = vmt::MerkleTree::new(data, ALGO);
+        let root = tree.get_root().to_vec();
+        VmtWrapper { tree: tree, root: root }
+    }
+
+    pub fn create_tree_with_map(data: &Vec<String>) -> VmtWrapper {
+        let tree = vmt::MerkleTree::new_with_map(data, ALGO);
+        let root = tree.get_root().to_vec();
+        VmtWrapper { tree: tree, root: root }
     }
 }
 
 impl TreeWrapper<String> for VmtWrapper {
-    fn find(value: &String) {
-//unimplemented!()
+    fn find(&self, value: &String) {
+        self.tree.build_proof(value);
     }
 
-    fn find_and_validate(value: &String) {
-//unimplemented!()
+    fn find_and_validate(&self, value: &String) {
+        let result: Option<Vec<&[u8]>> = self.tree.build_proof(value);
+        assert_eq!(true, self.tree.validate(value, result.unwrap(), &self.root[..]));
     }
 }
